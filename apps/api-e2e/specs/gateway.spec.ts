094c6fbfc27811cca09456ec0a3ce4d371088199
crisdegraciadev <crisdegraciadev@gmail.com>
2024-06-06 21:14:05 +0200

    first commit

---
+++ apps/api-e2e/specs/gateway.spec.ts
@@ -0,0 +1,11 @@
+import supertest from "supertest";
+
+const BASE_URL = "http://localhost:3001";
+const request = supertest(BASE_URL);
+
+describe("GATEWAY", () => {
+  it("GET /", async () => {
+    const { statusCode } = await request.get("");
+    expect(statusCode).toBe(200);
+  });
+});
