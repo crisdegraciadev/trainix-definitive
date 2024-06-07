67a647a86897244f408265b093243c90005d5182
crisdegraciadev <crisdegraciadev@gmail.com>
2024-06-07 12:35:27 +0200

    feat(general): add containers and e2e test scripts

--- apps/api-e2e/specs/gateway.spec.ts
+++ apps/api-e2e/specs/gateway.spec.ts
@@ -6,6 +6,7 @@ const request = supertest(BASE_URL);
 describe("GATEWAY", () => {
   it("GET /", async () => {
     const { statusCode } = await request.get("");
+    console.log({ statusCode });
     expect(statusCode).toBe(200);
   });
 });
